6cf648c8a6f2b1b725b88b1cb3567d4051a2ab8e
MJP <markangelpenaranda@gmail.com>
2017-01-30 14:16:08 +0800

    Add TimeZone Coordinates

---
+++ src/TimeZoneClient.php
@@ -0,0 +1,29 @@
+<?php
+
+class TimeZoneClient 
+{
+
+	public static function getTimeZoneByCoordinates(long $long, long $lat, $googleMapApiKey)
+	{
+		$url = "https://maps.googleapis.com/maps/api/timezone/json?location=". $lat .",". $long ."&timestamp=" . $time() ."&key=" . $apiKey;
+
+		$curl = curl_init();
+		
+		$action_url = $url;
+
+
+
+		curl_setopt_array($curl, array(
+			    CURLOPT_RETURNTRANSFER => 1,
+			    CURLOPT_URL => $url
+			));
+		$resp = curl_exec($curl);
+
+
+       $result = json_decode($resp, true);
+
+		return $result;		
+	
+	}
+
+}
