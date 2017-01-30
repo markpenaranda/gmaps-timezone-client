ca89e1271b70752a3f107ddc30a2157ed33d1da3
MJP <markangelpenaranda@gmail.com>
2017-01-30 14:36:13 +0800

    Data Type Coordinates

--- src/MarkPenaranda/GMapsTimeZone/TimeZoneClient.php
+++ src/MarkPenaranda/GMapsTimeZone/TimeZoneClient.php
@@ -4,7 +4,7 @@ namespace MarkPenaranda\GMapsTimeZone;
 class TimeZoneClient 
 {
 
-	public static function getTimeZoneByCoordinates(float $long, float $lat, $googleMapApiKey)
+	public static function getTimeZoneByCoordinates($long, $lat, $googleMapApiKey)
 	{
 		$url = "https://maps.googleapis.com/maps/api/timezone/json?location=". $lat .",". $long ."&timestamp=" . $time() ."&key=" . $apiKey;
 
